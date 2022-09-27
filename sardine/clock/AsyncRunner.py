EmmaleeBonderud0325/sardine

commit cce8d3b3683d1019ebc04fb22e9d627c9f4d5d8a

Defer calls by 1*ppqn with drift correction
This replaces the previous deferred scheduling implementation which was
phase-dependent with a more static delay. The new deferred scheduling
can correct for any latency that comes from `asyncio.wait()`.

sardine/clock/AsyncRunner.py
@@ -304,17 +304,25 @@ class AsyncRunner:
                     + self._get_corrected_interval(delay, delta_correction=True)
                 )
 
-                # print(
-                #     f'{self.clock} AR [green]'
-                #     f'expected: {self._expected_interval}, previous: {self.clock.tick}, '
-                #     f'delta: {self._delta}, shift: {self.interval_shift}'
-                # )
+                start = self.clock.tick
 
-                handle = asyncio.ensure_future(self._wait_beats(delay))
-                reload = asyncio.ensure_future(self._reload_event.wait())
+                handle = self._wait_beats(delay)
+                reload = self._reload_event.wait()
                 done, pending = await asyncio.wait(
-                    (handle, reload), return_when=asyncio.FIRST_COMPLETED
+                    (asyncio.ensure_future(handle), asyncio.ensure_future(reload)),
+                    return_when=asyncio.FIRST_COMPLETED,
                 )
+
+                # For deferred scheduling we'll record any drift after asyncio.wait,
+                # which tends to add about 1 tick of latency with ppqn = 24
+                sleep_drift = self.clock.tick - handle.when
+
+                # print(
+                #     f"{self.clock} AR [green]"
+                #     f"expected: {self._expected_interval}, previous: {start}, "
+                #     f"delta: {self._delta}, shift: {self.interval_shift}, "
+                #     f"post drift: {sleep_drift}"
+                # )
 
                 for fut in pending:
                     fut.cancel()
@@ -325,7 +333,7 @@ class AsyncRunner:
                 try:
                     # Use copied context in function by creating it as a task
                     await asyncio.create_task(
-                        self._call_func(state.func, args, kwargs),
+                        self._call_func(sleep_drift, state.func, args, kwargs),
                         name=f"asyncrunner-func-{name}",
                     )
                 except Exception as e:
@@ -364,13 +372,13 @@ class AsyncRunner:
         with self.clock._scoped_tick_shift(self.interval_shift - delta - offset):
             return self.clock.get_beat_ticks(delay) - delta
 
-    async def _call_func(self, func, args, kwargs):
+    async def _call_func(self, delta: int, func, args, kwargs):
         """Calls the given function and optionally applies an initial
         tick shift of 1 beat when the `deferred` attribute is
         set to True.
         """
         if self.deferred:
-            ticks = self._get_corrected_interval(1, offset=-1)
+            ticks = 1 * self.clock.ppqn - delta
             self.clock.tick_shift += ticks
 
         return await _maybe_coro(func, *args, **kwargs)

commit d4728c6ff06280178b43cf548aecf2be8a129c77

Fix function reloading regression from cce8d3b

sardine/clock/AsyncRunner.py
@@ -364,9 +364,9 @@ class AsyncRunner:
                 # start = self.clock.tick
 
                 handle = self._wait_beats(delay)
-                reload = self._reload_event.wait()
+                reload_task = asyncio.ensure_future(self._reload_event.wait())
                 done, pending = await asyncio.wait(
-                    (asyncio.ensure_future(handle), asyncio.ensure_future(reload)),
+                    (asyncio.ensure_future(handle), reload_task),
                     return_when=asyncio.FIRST_COMPLETED,
                 )
 
@@ -381,7 +381,7 @@ class AsyncRunner:
 
                 for fut in pending:
                     fut.cancel()
-                if reload in done:
+                if reload_task in done:
                     self.swim()
                     continue
 